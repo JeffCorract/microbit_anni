d8295a6ebf1a518749b123e52cad21cf53f6d222
JeffCorract <jeffcorract@gmail.com>
2022-09-28 10:01:08 +0800

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+bluetooth.startAccelerometerService()
+bluetooth.startButtonService()
+bluetooth.startIOPinService()
+bluetooth.startTemperatureService()
+bluetooth.startMagnetometerService()
 basic.forever(function () {
-	
+    basic.showLeds(`
+        . . . . #
+        . . . # .
+        # . # . .
+        . # . . .
+        . . . . .
+        `)
 })
